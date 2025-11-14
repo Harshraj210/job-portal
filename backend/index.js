d9714bb6be225c72682edd5f46734ed6c3a46074
Akshit Aman <amanakshit1234@gmail.com>
2025-11-15 01:51:03 +0530

    Added cookie-parser middleware and cors configuration

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
@@ -11,20 +12,31 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 
+// Middleware
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 
 app.use("/api/jobs", jobRoutes);
+
 app.get('/', (req, res) => {
-  res.send('Server running');
+  return res
+    .status(200)
+    .json({ message: "Welcome to the Job Portal Backend API" });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port : ${port}`);
+  console.log(`Server is running at port : ${port}`);
 });
